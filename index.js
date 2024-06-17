2a56386b48264879ecc087dc9f656f16eb5807ff
Swasti Bhagora <64397504+Swasti877@users.noreply.github.com>
2024-06-17 10:27:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 const port = 5000 || process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.post("/api/successfullyPayment", (req, res) => {
   console.log('req >> ', req);
